ebdbed6a79929f759715e0ea9f85dd9f07802008
Mauro Crispim <70303414+mcrispim@users.noreply.github.com>
2022-07-29 13:10:45 -0300

    Adiciona um recycler view no layout do fragmento de News

--- app/src/main/java/com/example/diosoccernews/ui/news/NewsFragment.kt
+++ app/src/main/java/com/example/diosoccernews/ui/news/NewsFragment.kt
@@ -4,7 +4,6 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import com.example.diosoccernews.databinding.FragmentNewsBinding
@@ -22,16 +21,14 @@ class NewsFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        val homeViewModel =
+        val newsViewModel =
             ViewModelProvider(this).get(NewsViewModel::class.java)
 
         _binding = FragmentNewsBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-        val textView: TextView = binding.textHome
-        homeViewModel.text.observe(viewLifecycleOwner) {
-            textView.text = it
-        }
+
+
         return root
     }
 
