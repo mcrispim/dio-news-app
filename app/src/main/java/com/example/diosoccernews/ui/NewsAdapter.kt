3028f90d9497f6b587069117e75023b7e9819535
Mauro Crispim <70303414+mcrispim@users.noreply.github.com>
2022-07-29 14:57:24 -0300

    Cria o adapter para as News

---
+++ app/src/main/java/com/example/diosoccernews/ui/NewsAdapter.kt
@@ -0,0 +1,54 @@
+package com.example.diosoccernews.ui
+
+import android.content.Intent
+import android.net.Uri
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.diosoccernews.data.News
+import com.example.diosoccernews.databinding.ItemNewsBinding
+import com.squareup.picasso.Picasso
+
+class NewsAdapter(private val listNews: List<News>, private val favoriteListener: View.OnClickListener): RecyclerView.Adapter<NewsAdapter.NewsViewHolder>() {
+
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): NewsViewHolder {
+                val layoutInflater = LayoutInflater.from(parent.context)
+                val binding = ItemNewsBinding.inflate(layoutInflater, parent, false)
+                return NewsViewHolder(binding)
+        }
+
+        override fun onBindViewHolder(holder: NewsViewHolder, position: Int) {
+                val news = listNews[position]
+                Picasso.get()
+                        .load(news.imagem)
+                        .into(holder.binding.ivImagem)
+                holder.binding.tvTitulo.text = news.titulo
+                holder.binding.tvTexto.text = news.texto
+
+                // implementação da funcionalidade Link para a Notícia
+                holder.binding.btLink.setOnClickListener { _ ->
+                        val intentOpenURL = Intent(Intent.ACTION_VIEW)
+                        intentOpenURL.data = Uri.parse(news.link)
+                        holder.itemView.context.startActivity(intentOpenURL)
+                }
+
+                // implementação da funcionalidade Share da Notícia
+                holder.binding.ivShare.setOnClickListener { _ ->
+                        val intentShareURL = Intent(Intent.ACTION_SEND)
+                        intentShareURL.type ="text/plain"
+                        intentShareURL.putExtra(Intent.EXTRA_TEXT, news.link)
+                        holder.itemView.context.startActivity(Intent.createChooser(intentShareURL, "Share via"))
+                }
+
+                // Implementação da funcionalidade favorito da notícia
+                holder.binding.ivFavorito.setOnClickListener(favoriteListener)
+        }
+
+        override fun getItemCount() = listNews.size
+
+        inner  class NewsViewHolder(val binding: ItemNewsBinding): RecyclerView.ViewHolder(binding.root) {
+
+        }
+
+}
