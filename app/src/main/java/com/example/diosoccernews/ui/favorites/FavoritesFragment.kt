da7aa502563f0d7397b51bf80d2a59783de813bf
Mauro Crispim <70303414+mcrispim@users.noreply.github.com>
2022-07-29 11:11:03 -0300

    Realiza pequenas correções

--- app/src/main/java/com/example/diosoccernews/ui/favorites/FavoritesFragment.kt
+++ app/src/main/java/com/example/diosoccernews/ui/favorites/FavoritesFragment.kt
@@ -7,11 +7,11 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
-import com.example.diosoccernews.databinding.FragmentNotificationsBinding
+import com.example.diosoccernews.databinding.FragmentFavoritesBinding
 
 class FavoritesFragment : Fragment() {
 
-    private var _binding: FragmentNotificationsBinding? = null
+    private var _binding: FragmentFavoritesBinding? = null
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -25,7 +25,7 @@ class FavoritesFragment : Fragment() {
         val notificationsViewModel =
             ViewModelProvider(this).get(FavoritesViewModel::class.java)
 
-        _binding = FragmentNotificationsBinding.inflate(inflater, container, false)
+        _binding = FragmentFavoritesBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
         val textView: TextView = binding.textNotifications
--- app/src/main/java/com/example/diosoccernews/ui/news/NewsFragment.kt
+++ app/src/main/java/com/example/diosoccernews/ui/news/NewsFragment.kt
@@ -7,11 +7,11 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
-import com.example.diosoccernews.databinding.FragmentHomeBinding
+import com.example.diosoccernews.databinding.FragmentNewsBinding
 
 class NewsFragment : Fragment() {
 
-    private var _binding: FragmentHomeBinding? = null
+    private var _binding: FragmentNewsBinding? = null
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -25,7 +25,7 @@ class NewsFragment : Fragment() {
         val homeViewModel =
             ViewModelProvider(this).get(NewsViewModel::class.java)
 
-        _binding = FragmentHomeBinding.inflate(inflater, container, false)
+        _binding = FragmentNewsBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
         val textView: TextView = binding.textHome
