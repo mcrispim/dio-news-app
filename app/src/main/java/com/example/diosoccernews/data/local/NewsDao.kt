339893aadafc776ee2983efb581fd28d44c25257
Mauro Crispim <70303414+mcrispim@users.noreply.github.com>
2022-08-01 14:00:48 -0300

    Acrescenta addNews() no NewsDao

--- app/src/main/java/com/example/diosoccernews/data/local/NewsDao.kt
+++ app/src/main/java/com/example/diosoccernews/data/local/NewsDao.kt
@@ -1,5 +1,7 @@
 package com.example.diosoccernews.data.local
 
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
 import androidx.room.Query
 import com.example.diosoccernews.data.News
 
@@ -9,4 +11,7 @@ interface NewsDao {
 
     @Query("SELECT * FROM news WHERE isFavorite = 1")
     fun getFavoriteNews(favorite: Boolean): List<News>
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    fun addNews(news: News)
 }
